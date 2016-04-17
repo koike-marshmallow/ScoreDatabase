32416281497b78f276658eda8645ee8baf3f0536
takaaki koike <taka.vao.321@gmail.com>
2016-04-17 21:30:57 +0900

    editorviewを作成

--- projects/ScoreDatabase/src/jp/tkch/sdtool/gui/ScoreDataEditorView.java
+++ projects/ScoreDatabase/src/jp/tkch/sdtool/gui/ScoreDataEditorView.java
@@ -28,14 +28,24 @@ implements ActionListener{
 		"確定", "続ける", "キャンセル"
 	};
 
-	private ScoreData sdata;
 	private JTextField[] tfields;
 	private JButton[] buttons;
+	private ScoreDataEditorViewListener listener;
 
 
 
 	public ScoreDataEditorView() {
 		buildFrame();
+	}
+
+	public ScoreDataEditorView(ScoreData sdata){
+		this();
+		setScoreData(sdata);
+	}
+
+	public ScoreDataEditorView(ScoreData sdata, ScoreDataEditorViewListener l0){
+		this(sdata);
+		setListener(l0);
 	}
 
 	private void buildFrame(){
@@ -78,13 +88,68 @@ implements ActionListener{
 		}
 	}
 
-	public static void main(String[] args){
-		ScoreDataEditorView view = new ScoreDataEditorView();
-		view.setVisible(true);
+	public void setScoreData(ScoreData sd){
+		tfields[0].setText(String.valueOf(sd.getId()));
+		tfields[1].setText(sd.getTitle());
+		tfields[2].setText(sd.getIndex());
+		tfields[3].setText(sd.getParameter("author"));
+		tfields[4].setText(sd.getParameter("publisher"));
+		tfields[5].setText(sd.getParameter("comment"));
 	}
+
+	public ScoreData getScoreData(){
+		ScoreData input = new ScoreData();
+		try{
+			input.setId(Integer.parseInt(tfields[0].getText()));
+		}catch(NumberFormatException e){
+			input.setId(0);
+		}
+		input.setTitle(tfields[1].getText());
+		input.setIndex(tfields[2].getText());
+		input.setParameter("author", tfields[3].getText());
+		input.setParameter("publisher", tfields[4].getText());
+		input.setParameter("comment", tfields[5].getText());
+
+		return input;
+	}
+
+	public void setListener(ScoreDataEditorViewListener l0){
+		listener = l0;
+	}
+
+
+	public void setIdEditable(boolean b0){
+		tfields[0].setEditable(b0);
+	}
+
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO 自動生成されたメソッド・スタブ
+		int code = 0;
+		Object source = e.getSource();
+
+		if( source == buttons[0] ){
+			code = REGIST;
+		}else if( source == buttons[1] ){
+			code = CONTINUE;
+		}else if( source == buttons[2] ){
+			code = CANCEL;
+		}
+
+		if( listener != null ){
+			listener.inputCompleted(code, getScoreData());
+		}
+	}
+
+
+	public static void main(String[] args){
+		ScoreDataEditorView view = new ScoreDataEditorView(new ScoreData(1001, "ユメヘノボウケン"));
+		view.setListener(new ScoreDataEditorViewListener(){
+			public void inputCompleted(int c, ScoreData sd){
+				System.out.println("[" + c + "] " + sd.getTitle());
+			}
+		});
+		view.setVisible(true);
 	}
 }
---
+++ projects/ScoreDatabase/src/jp/tkch/sdtool/gui/ScoreDataEditorViewListener.java
@@ -0,0 +1,7 @@
+package jp.tkch.sdtool.gui;
+
+import jp.tkch.sdtool.model.ScoreData;
+
+public interface ScoreDataEditorViewListener {
+	public void inputCompleted(int code, ScoreData sdata);
+}
