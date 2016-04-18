a069f8273099124775a09cc43d384be2030d6026
takaaki koike <taka.vao.321@gmail.com>
2016-04-18 23:14:55 +0900

    CSSの準備

---
+++ projects/ScoreDatabase/src/jp/tkch/htmlbuilder/css/CSSBlock.java
@@ -0,0 +1,50 @@
+package jp.tkch.htmlbuilder.css;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSSBlock {
+	private String selector;
+	private List<CSSProperty> properties;
+
+	public CSSBlock(){
+		selector = "";
+		properties = new ArrayList<CSSProperty>();
+	}
+
+	public CSSBlock(String s0){
+		selector = s0;
+	}
+
+	public String getSelector() {
+		return selector;
+	}
+
+	public void setSelector(String selector) {
+		this.selector = selector;
+	}
+
+	public void addProperty(CSSProperty p0){
+		properties.add(p0);
+	}
+
+	public void clearProperty(){
+		properties.clear();
+	}
+
+	public boolean isValid(){
+		return (selector != null && !selector.equals(""));
+	}
+
+	public String toString(){
+		String str = "";
+		if( isValid() ){
+			str = selector + "{";
+			for(CSSProperty property : properties){
+				str += property.toString();
+			}
+			str += "}";
+		}
+		return str;
+	}
+}
