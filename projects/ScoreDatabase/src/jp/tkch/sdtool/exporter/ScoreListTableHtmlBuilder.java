85cbecd053391d91036d42e2ca51a4e1c68af0d6
kioke takaaki <taka.vao.321@gmail.com>
2016-04-21 20:26:21 +0900

    途中

---
+++ projects/ScoreDatabase/src/jp/tkch/sdtool/exporter/ScoreListTableHtmlBuilder.java
@@ -0,0 +1,15 @@
+package jp.tkch.sdtool.exporter;
+
+import jp.tkch.sdtool.model.ScoreDataList;
+
+public class ScoreListTableHtmlBuilder {
+	ScoreDataList list;
+	String labels;
+	int[] width;
+
+	public ScoreListTableHtmlBuilder(){
+		list = null;
+		labels = null;
+		width = null;
+	}
+}
