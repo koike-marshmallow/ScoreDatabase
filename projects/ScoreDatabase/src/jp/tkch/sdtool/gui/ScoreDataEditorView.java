8a8fc00dda738a75c95b41ca3ea10bf6f7dafe64
takaaki koike <taka.vao.321@gmail.com>
2016-04-22 23:32:05 +0900

    ×で勝手に終了しないように修正

--- projects/ScoreDatabase/src/jp/tkch/sdtool/gui/ScoreDataEditorView.java
+++ projects/ScoreDatabase/src/jp/tkch/sdtool/gui/ScoreDataEditorView.java
@@ -49,7 +49,7 @@ implements ActionListener{
 	}
 
 	private void buildFrame(){
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 480, 320);
 
 		JPanel pTop = new JPanel(new BorderLayout());
