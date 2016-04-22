4173ea3b02419e9526334ea88199fed3bc126226
koike takaaki <taka.vao.321@gmail.com>
2016-04-22 16:23:33 +0900

    ctype作成中

--- projects/ScoreDatabase/src/jp/tkch/sdtool/jindex/JindexCtype.java
+++ projects/ScoreDatabase/src/jp/tkch/sdtool/jindex/JindexCtype.java
@@ -9,6 +9,42 @@ import java.util.List;
 import java.util.Locale;
 
 public class JindexCtype {
+	public static final int CTYPE_NUM = 101;
+	public static final int CTYPE_ALPH = 102;
+	public static final int CTYPE_HIRAGANA = 103;
+	public static final int CTYPE_KANAKANA = 104;
+	public static final int CTYPE_KANJI = 105;
+	public static final int CTYPE_OTHERS = 106;
+	
+	public static int of(char c){
+		int block = UnicodeBlock.of(c);
+		
+		switch( block ){
+		case UnicodeBlock.HIRAGANA:
+			return CTYPE_HIRAGANA;
+		case UnicodeBlock.KATAKANA:
+			return CTYPE_KATAKANA;
+		case UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS:
+			return CTYPE_KANJI;
+		case UnicodeBlock.BASIC_LATIN:
+			if( c >= '0' && c <= '9' ){
+				return CTYPE_NUM;
+			}else if( 
+				(c >= 'A' && c <= 'Z') ||
+				(c >= 'a' && c <= 'z')
+			){
+				return CTYPE_ALPH;
+			}else{
+				return CTYPE_OTHERS;
+			}
+		default:
+			return CTYPE_OTHERS;
+		}
+		
+		return 0;
+	}
+
+
 	public static void main(String[] args){
 		String str = "あぁアァ今a1@Ａ";
 		List<Character> cs = new ArrayList<Character>();
